f0738cdd91a0814fa28dddd7fa61731438ca1dda
Jhon Aricapa <jhon.aricapa@correounivalle.edu.co>
2025-07-06 11:34:03 -0500

    feat: Implement admin dashboard layout and components; add sales data visualization and statistics cards for enhanced user experience

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,15 @@
-import Link from "next/link";
+import { Suspense } from "react";
+import SellsCardSection from "../components/app/admin/dashboard/SellsCardSection";
+import CompareSellsGraph from "../components/app/admin/dashboard/CompareSellsGraph";
+import "./page.css";
 
-export default function AdminHomePage() {
+export default async function AdminHomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
-      <p className="text-lg mb-6">This is the main page of your application.</p>
-      <Link
-        href="/auth"
-        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-      >
-        Welcome to CDA Moto GP
-      </Link>
+    <div className="admin-home-page">
+      <SellsCardSection />
+      <Suspense fallback={<div>Cargando ...</div>}>
+        <CompareSellsGraph />
+      </Suspense>
     </div>
   );
 }
---
+++ src/app/components/app/admin/dashboard/SellsCardSection.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React from "react";
+import { DollarOutlined, PieChartOutlined } from "@ant-design/icons";
+import { DatePicker, Divider, Typography } from "antd";
+import "@/app/asesor/page.css";
+
+type Props = {};
+
+const { Title, Text } = Typography;
+
+export default function SellsCardSection({}: Props) {
+  return (
+    <section className="grid-area-sells-cards grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
+      <article className="asesor-stat-card">
+        <PieChartOutlined className="icon utility" />
+        <span>
+          <Text type="secondary">Cantidad de ventas</Text>
+          <Text className="stat-growth negative" type="secondary">
+            -15%
+          </Text>
+        </span>
+        <Title level={3} style={{ margin: 0 }}>
+          500
+        </Title>
+      </article>
+      <article className="asesor-stat-card">
+        <DollarOutlined className="icon earning" />
+        <span>
+          <Text type="secondary">Total en ventas</Text>
+          <Text className="stat-growth" type="secondary">
+            +25%
+          </Text>
+        </span>
+        <Title level={3} style={{ margin: 0 }}>
+          $ 2900000
+        </Title>
+      </article>
+      <article className="asesor-stat-card">
+        <span className="flex items-center justify-between w-64">
+        <Text type="secondary">Filtrar por mes</Text>
+        <DatePicker placeholder="Seleccionar mes" picker="month" />
+        </span>
+        <Divider />
+        <Text type="secondary">Periodo</Text>
+        <Title level={3} style={{ margin: 0 }}>
+          Ultimos 4 meses
+        </Title>
+      </article>
+    </section>
+  );
+}
